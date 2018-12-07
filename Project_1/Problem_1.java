70a620c03897faa55e9868d5b05817d0566cf5f3
Anand_Shrivastava <anandshrivastava04@gmail.com>
2018-12-07 11:51:11 +0530

    Changes in project_1

--- Project_1/Problem_1.java
+++ Project_1/Problem_1.java
@@ -1,17 +1,27 @@
+
+//import  all neccesory files
 import java.io.*;
 import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
+//Create new class having same name as source file name
 public class Problem_1 {
 
     public static void main(String[] args) {
+      //create scanner to take inut
        Scanner sc = new Scanner(System.in);
+       //Create variable to accept input
         long t = sc.nextLong();
+        //For loop having main logic
         for(long i=0;i<t;i++){
+          //scan input and store it into variable
             long n = sc.nextLong();
+            //create local variabe
             long a=0, b=0, d=0;
+            //main logic goes here
             a=(n-1)%3;
             a=n-1-a;
             a=a/3;
@@ -22,6 +32,7 @@ public class Problem_1 {
             d=n-1-d;
             d=d/15;
             long c= 3*a*(a+1)/2 + 5*b*(b+1)/2 - 15*d*(d+1)/2;
+            //Generate output
             System.out.println(c);
         }
     }
